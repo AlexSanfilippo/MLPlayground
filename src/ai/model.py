67aed5113036bcfc1014c2eb2a32a9c02abcc5a0
cafalchio <maolivei@tcd.ie>
2025-09-08 22:40:16 +0100

    added simple training loop and simple physics interface

--- src/ai/model.py
+++ src/ai/model.py
@@ -1,5 +1,32 @@
 from torch import nn
+import torch
 
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print(f"{device=}")
 
-class ExampleModel(nn.Module):
-    pass
+class SimpleModel(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.linear = nn.Sequential(
+            nn.Linear(3, 64),
+            nn.ReLU(),
+            nn.Linear(64,64),
+            nn.ReLU(),
+            nn.Linear(64, 3)
+        )
+
+    def forward(self, x):
+        x = self.linear(x)
+        return x
+
+def create_model():
+    """Just to check if pytorch installation is working"""
+    model = SimpleModel().to(device)
+    return model
+
+if __name__ == "__main__":
+    model = create_model()
+    X = torch.rand(1, 3, device=device)
+    logits = model(X)
+    print(f"{logits=}")
+    print(model)
